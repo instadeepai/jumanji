db6574468d23a7c8ed544d0b85c2f9b65383ea1b
Clément Bonnet <c.bonnet@instadeep.com>
2021-11-25 11:20:59 +0000

    Resolve "Implement `swap` environment"

---
+++ jumanji/jax/swap/types.py
@@ -0,0 +1,27 @@
+from enum import IntEnum
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from dataclasses import dataclass
+else:
+    from chex import dataclass
+
+from chex import Array, PRNGKey
+
+Action = Array
+
+
+@dataclass
+class State:
+    key: PRNGKey
+    agent_pos: Array
+    blue_pos: Array
+    red_pos: Array
+    step_count: int
+
+
+class Actions(IntEnum):
+    UP: int = 0
+    RIGHT: int = 1
+    DOWN: int = 2
+    LEFT: int = 3
