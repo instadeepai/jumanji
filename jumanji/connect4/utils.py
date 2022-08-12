fix: 32-bit and 64-bit precision conversion between numpy and jax

jumanji/connect4/utils.py
@@ -253,4 +253,4 @@ def get_action_mask(board: Board) -> Array:
         binary action mask
 
     """
-    return jnp.equal(board[0, :], 0).astype(jnp.int8)
+    return jnp.equal(board[0, :], 0).astype(jnp.bool_)

jumanji/specs.py
@@ -31,6 +31,8 @@ import chex
 import dm_env.specs
 import jax.numpy as jnp
 
+from jumanji.types import get_valid_dtype
+
 T = TypeVar("T")
 
 
@@ -105,7 +107,7 @@ class Array(Spec[chex.Array]):
         """
         super().__init__(name)
         self._shape = tuple(int(dim) for dim in shape)
-        self._dtype = jnp.dtype(dtype)
+        self._dtype = get_valid_dtype(dtype)
 
     def __repr__(self) -> str:
         return f"Array(shape={repr(self.shape)}, dtype={repr(self.dtype)}, name={repr(self.name)})"

jumanji/specs_test.py
@@ -396,7 +396,7 @@ class TestDiscreteArray:
         "arg_name, new_value",
         [
             ("num_values", 4),
-            ("dtype", jnp.int64),
+            ("dtype", jnp.int16),
             ("name", "something_else"),
         ],
     )

jumanji/testing/env_not_smoke_test.py
@@ -59,6 +59,5 @@ def test_random_select_action(fake_env: FakeEnvironment) -> None:
     env_state, timestep, extra = fake_env.reset(key1)
     action_1 = select_action(key2, timestep.observation)
     action_2 = select_action(key3, timestep.observation)
-    # TODO: uncomment after 64/32 bit issue is fixed
-    # fake_env.action_spec().validate(action_1)
-    assert not (action_1 == action_2).all()
+    fake_env.action_spec().validate(action_1)
+    assert not jnp.all(action_1 == action_2)