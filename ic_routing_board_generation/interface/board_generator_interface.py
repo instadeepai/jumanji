af11d8772ac78eaa868e631133cf65be494d51a1
Marta <marta.wolinska16@imperial.ac.uk>
2023-01-31 20:58:16 +0000

    Design board generator interface

---
+++ ic_routing_board_generation/interface/board_generator_interface.py
@@ -0,0 +1,25 @@
+from enum import Enum
+
+from ic_routing_board_generation.board_generator.board_generator_v2_0_0_rb import \
+    Board
+from ic_routing_board_generation.board_generator.board_generator_v1_1_2_rb import Board as BoardV1
+
+
+class BoardGenerators(str, Enum):
+    """Enum of implemented board generators."""
+    RANDY_V1 = "randy_v1"
+    RANDOM_ROUTE = "random route"
+    BFS = "BFS"
+
+
+class BoardGenerator:
+    """Maps BoardGeneratorType to class of generator."""
+    board_generator_dict = {
+        BoardGenerators.RANDY_V1: BoardV1,
+        BoardGenerators.RANDOM_ROUTE: Board,
+    }
+
+    @classmethod
+    def get_board_generator(cls, board_enum: BoardGenerators):
+        """Return class of desired board generator."""
+        return cls.board_generator_dict[board_enum]
