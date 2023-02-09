2e8b4288be46dbf6d1a304cc1059360dcf47001f
Marta <marta.wolinska16@imperial.ac.uk>
2023-02-09 15:51:00 +0000

    Add BFS board variations for benchmark
    Update old text in docs directory
    
    
    Add timing to benchmark

--- ic_routing_board_generation/interface/board_generator_interface.py
+++ ic_routing_board_generation/interface/board_generator_interface.py
@@ -1,6 +1,9 @@
 from enum import Enum
 
 from ic_routing_board_generation.board_generator.bfs_board import BFSBoard
+from ic_routing_board_generation.board_generator.bfs_board_variations import \
+    BFSBoardMinBends, BFSBoardFifo, BFSBoardShortest, BFSBoardLongest
+
 from ic_routing_board_generation.board_generator.board_generator_v1_1_2_rb import \
     BoardV0
 from ic_routing_board_generation.board_generator.board_generator_v2_0_0_rb import \
@@ -12,6 +15,10 @@ class BoardName(str, Enum):
     RANDOM_V0 = "random_v0"
     RANDOM_WALK = "random_walk"
     BFS_BASE = "BFS_base"
+    BFS_MIN_BENDS = "bfs_min_bend"
+    BFS_FIFO = "bfs_fifo"
+    BFS_SHORTEST = "bfs_short"
+    BFS_LONGEST = "bfs_long"
 
 
 class BoardGenerator:
@@ -20,6 +27,10 @@ class BoardGenerator:
         BoardName.RANDOM_V0: BoardV0,
         BoardName.RANDOM_WALK: RandomWalkBoard,
         BoardName.BFS_BASE: BFSBoard,
+        BoardName.BFS_MIN_BENDS: BFSBoardMinBends,
+        BoardName.BFS_FIFO: BFSBoardFifo,
+        BoardName.BFS_SHORTEST: BFSBoardShortest,
+        BoardName.BFS_LONGEST: BFSBoardLongest,
     }
 
     @classmethod
