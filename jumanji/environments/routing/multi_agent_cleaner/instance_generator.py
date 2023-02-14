29af73848fd0fbb781eb310665c581cfb10d73e3
Vincent Coyette <97098456+coyettev@users.noreply.github.com>
2023-02-14 16:34:35 +0100

    feat(cleaner): random instance generator (#341)
    Co-authored-by: Daniel <57721552+dluo96@users.noreply.github.com>

---
+++ jumanji/environments/routing/multi_agent_cleaner/instance_generator.py
@@ -0,0 +1,221 @@
+# Copyright 2022 InstaDeep Ltd. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Random instance generator for the Cleaner environment.
+
+The algorithm used to generate a maze is called the recursive division method:
+
+> Begin with the maze's space with no walls. Call this a chamber. Divide the chamber with a
+> randomly positioned wall (or multiple walls) where each wall contains a randomly positioned
+> passage opening within it. Then recursively repeat the process on the subchambers until all
+> chambers are minimum sized.
+
+(from [wikipedia](https://en.wikipedia.org/wiki/Maze_generation_algorithm))
+
+It is modified to be jit-table. The ending condition of recursive function in Jax cannot depend
+on abstract tensor values. Hence, instead of using recursion, a `Stack` is used to keep track
+of the remaining chambers to split. While the stack is not empty, pop a chamber from the stack,
+split it, and push the two newly created subchambers on the stack if they are not of minimum size,
+i.e. of shape 1 by 1.
+
+Unlike the graph based maze representation where a wall between two cells corresponds to the lack of
+an edge between the two associated nodes, walls have a thickness of 1 in this pixel based
+representation. Because of this, vertical walls will have an odd x coordinate while horizontal walls
+will have an odd y coordinate. It also means that a passage (corresponding to an edge between two
+nodes) through a vertical wall must be at an even y coordinate while a passage through a horizontal
+wall must be at an even x coordinate.
+"""
+from typing import NamedTuple, Tuple
+
+import chex
+import jax
+import jax.numpy as jnp
+from typing_extensions import TypeAlias
+
+from jumanji.environments.routing.multi_agent_cleaner.constants import DIRTY, WALL
+from jumanji.environments.routing.multi_agent_cleaner.stack import (
+    Stack,
+    create_stack,
+    empty_stack,
+    stack_pop,
+    stack_push,
+)
+
+Maze: TypeAlias = chex.Array
+
+
+class InstanceGeneratorState(NamedTuple):
+    """The state of the instance generator.
+
+    - maze: the maze containing the walls created so far.
+    - chambers: the stack of remaining chambers to split.
+    - key: the Jax random generation key.
+    """
+
+    maze: Maze
+    chambers: Stack
+    key: chex.PRNGKey
+
+
+def create_chambers_stack(maze_width: int, maze_height: int) -> Stack:
+    """Initialize the stack of chambers."""
+    max_num_chamber = maze_width * maze_height
+    # A chamber is defined by 4 digits: x0, y0, width, height
+    chambers = create_stack(max_num_chamber, 4)
+    # Initially only one chamber: the whole maze
+    return stack_push(chambers, jnp.array([0, 0, maze_width, maze_height]))
+
+
+def create_empty_maze(width: int, height: int) -> Maze:
+    """Create an empty maze."""
+    return jnp.full((height, width), DIRTY, dtype=jnp.int8)
+
+
+def random_even(key: chex.PRNGKey, max_val: int) -> chex.Array:
+    """Randomly draw an even integer between 0 (inclusive) and max_val (exclusive)."""
+    return jax.random.randint(key, (), 0, (max_val + 1) // 2) * 2
+
+
+def random_odd(key: chex.PRNGKey, max_val: int) -> chex.Array:
+    """Randomly draw an odd integer between 0 (inclusive) and max_val (exclusive)."""
+    return jax.random.randint(key, (), 0, max_val // 2) * 2 + 1
+
+
+def draw_horizontal_wall(maze: Maze, x: int, y: int, width: int) -> Maze:
+    """Draw a horizontal wall on the maze starting from (x,y) with the specified width."""
+
+    def body_fun(i: int, maze: Maze) -> Maze:
+        return maze.at[y, i].set(WALL)
+
+    return jax.lax.fori_loop(x, x + width, body_fun, maze)
+
+
+def draw_vertical_wall(maze: Maze, x: int, y: int, height: int) -> Maze:
+    """Draw a vertical wall on the maze starting from (x,y) with the specified height."""
+
+    def body_fun(i: int, maze: Maze) -> Maze:
+        return maze.at[i, x].set(WALL)
+
+    return jax.lax.fori_loop(y, y + height, body_fun, maze)
+
+
+def create_chamber(chambers: Stack, x: int, y: int, width: int, height: int) -> Stack:
+    """Create a new chamber from (x,y) and a given width and height.
+
+    If the new chamber is less then the minimum size (1 by 1), then do nothing.
+    """
+    new_stack: Stack = jax.lax.cond(
+        (width > 1) & (height > 1),
+        lambda c: stack_push(c, jnp.array([x, y, width, height])),
+        lambda c: c,
+        chambers,
+    )
+    return new_stack
+
+
+def split_vertically(
+    state: InstanceGeneratorState, chamber: chex.Array
+) -> InstanceGeneratorState:
+    """Split the chamber vertically.
+
+    Randomly draw a horizontal wall to split the chamber vertically. Randomly open a passage
+    within this wall, and push the two newly created sub-chambers to the stack if they are not
+    of minimum size.
+    """
+    x, y, width, height = chamber
+    key, wall_key, passage_key = jax.random.split(state.key, num=3)
+
+    # Randomly draw a vertical wall to split the chamber
+    wall_y = random_odd(wall_key, height)
+    maze = draw_horizontal_wall(state.maze, x, wall_y, width)
+
+    # Create chambers above and below the wall
+    chambers = create_chamber(state.chambers, x, y, width, wall_y - y)
+    chambers = create_chamber(chambers, x, wall_y + 1, width, height - wall_y - y - 1)
+
+    # Randomly position a passage opening within the wall
+    passage_x = random_even(passage_key, width)
+    maze = maze.at[wall_y, x + passage_x].set(DIRTY)
+
+    return InstanceGeneratorState(maze, chambers, key)
+
+
+def split_horizontally(
+    state: InstanceGeneratorState, chamber: chex.Array
+) -> Tuple[Maze, Stack, chex.PRNGKey]:
+    """Split the chamber horizontally.
+
+    Randomly draw a vertical wall to split the chamber horizontally. Randomly open a passage
+    within this wall, and push the two newly created sub-chambers to the stack if they are not
+    of minimum size.
+    """
+    x, y, width, height = chamber
+    key, wall_key, passage_key = jax.random.split(state.key, num=3)
+
+    # Randomly draw a vertical wall to split the chamber
+    wall_x = random_odd(wall_key, width)
+    maze = draw_vertical_wall(state.maze, wall_x, y, height)
+
+    # Create chambers left and right of the wall
+    chambers = create_chamber(state.chambers, x, y, wall_x - x, height)
+    chambers = create_chamber(chambers, wall_x + 1, y, width - wall_x - x - 1, height)
+
+    # Randomly position a passage opening withing the wall
+    passage_y = random_even(passage_key, height)
+    maze = maze.at[y + passage_y, wall_x].set(DIRTY)
+
+    return InstanceGeneratorState(maze, chambers, key)
+
+
+def split_next_chamber(state: InstanceGeneratorState) -> InstanceGeneratorState:
+    """Split the next chamber on top of the stack."""
+    chambers, chamber = stack_pop(state.chambers)
+    *_, width, height = chamber
+
+    new_state: InstanceGeneratorState = jax.lax.cond(
+        width >= height,
+        split_horizontally,
+        split_vertically,
+        InstanceGeneratorState(state.maze, chambers, state.key),
+        chamber,
+    )
+    return new_state
+
+
+def chambers_remaining(state: InstanceGeneratorState) -> int:
+    """Check if there is any chamber remaining to split."""
+    return ~empty_stack(state.chambers)
+
+
+def generate_maze(width: int, height: int, key: chex.PRNGKey) -> Maze:
+    """Randomly generate a maze.
+
+    Args:
+        width: the width of the maze to create.
+        height: the height of the maze to create.
+        key: the Jax random number generation key.
+
+    Returns:
+        maze: the generated maze.
+    """
+    maze = create_empty_maze(width, height)
+    chambers = create_chambers_stack(width, height)
+
+    initial_state = InstanceGeneratorState(maze, chambers, key)
+
+    final_state = jax.lax.while_loop(
+        chambers_remaining, split_next_chamber, initial_state
+    )
+
+    return final_state.maze
---
+++ jumanji/environments/routing/multi_agent_cleaner/instance_generator_test.py
@@ -0,0 +1,159 @@
+# Copyright 2022 InstaDeep Ltd. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import chex
+import jax
+import jax.numpy as jnp
+import pytest
+import scipy
+
+from jumanji.environments.routing.multi_agent_cleaner.constants import DIRTY, WALL
+from jumanji.environments.routing.multi_agent_cleaner.instance_generator import (
+    InstanceGeneratorState,
+    Maze,
+    create_chambers_stack,
+    create_empty_maze,
+    generate_maze,
+    random_even,
+    random_odd,
+    split_horizontally,
+    split_vertically,
+)
+from jumanji.environments.routing.multi_agent_cleaner.stack import Stack, stack_pop
+
+
+def no_more_chamber(maze: Maze) -> chex.Array:
+    """Test if there is no chamber in the maze that can be divided anymore.
+
+    A chamber can be divided if its width and height are greater or equal to two.
+    To efficiently detect chambers of shape at least two by two in the maze, a convolution with
+    a positive kernel of size (2,2) can be used: if there is a chamber, its convolution with
+    the kernel will be 0. The contrapositive is that if there is no 0 in the convolution of
+    the maze and a positive kernel of size (2,2), then there is no chamber.
+    """
+    kernel = jnp.ones((2, 2))
+    convolved = jax.scipy.signal.convolve2d(maze, kernel, mode="valid")
+    return jnp.all(convolved)
+
+
+def all_tiles_connected(maze: Maze) -> bool:
+    """Test if all the tiles of the maze can be reached.
+
+    The function scipy.ndimage.label can be used to count the number of connected components
+    in an image. The background has to be composed of zero, and connected components are convex
+    shapes of positive integers pixels.
+    If the maze contain only one connected component, then all tiles can be reached from
+    all others.
+    """
+    # replace walls by 0 and dirty by 1
+    inverted_maze = -(maze - WALL) / WALL
+    _, n_connected_components = scipy.ndimage.label(inverted_maze)
+    return bool(n_connected_components == 1)
+
+
+class TestInstanceGenerator:
+    WIDTH = 5
+    HEIGHT = 7
+
+    @pytest.fixture
+    def maze(self) -> Maze:
+        return create_empty_maze(self.WIDTH, self.HEIGHT)
+
+    @pytest.fixture
+    def chambers(self) -> Stack:
+        return create_chambers_stack(self.WIDTH, self.HEIGHT)
+
+    @pytest.fixture
+    def key(self) -> chex.PRNGKey:
+        return jax.random.PRNGKey(0)
+
+    def test_create_chambers_stack(self) -> None:
+        chambers = create_chambers_stack(self.WIDTH, self.HEIGHT)
+        assert isinstance(chambers, Stack)
+        assert chambers.data.shape == (self.WIDTH * self.HEIGHT, 4)
+        # Initially only one chambers: the full maze
+        assert jnp.all(chambers.data[0] == jnp.array([0, 0, self.WIDTH, self.HEIGHT]))
+
+    def test_create_empty_maze(self) -> None:
+        maze = create_empty_maze(self.WIDTH, self.HEIGHT)
+        # Initially there is no wall
+        assert jnp.all(maze == DIRTY)
+        assert maze.shape == (self.HEIGHT, self.WIDTH)
+
+    def test_random_even(self, key: chex.PRNGKey) -> None:
+        max_val = 10
+        repeat = 10
+        for _ in range(repeat):
+            key, subkey = jax.random.split(key)
+            i = random_even(subkey, max_val)
+            assert i % 2 == 0
+            assert 0 <= i < max_val
+
+    def test_random_odd(self, key: chex.PRNGKey) -> None:
+        max_val = 10
+        repeat = 10
+        for _ in range(repeat):
+            key, subkey = jax.random.split(key)
+            i = random_odd(subkey, max_val)
+            assert i % 2 == 1
+            assert 0 <= i < max_val
+
+    def test_split_vertically(
+        self, maze: Maze, chambers: Stack, key: chex.PRNGKey
+    ) -> None:
+        """Test that a horizontal wall is drawn and that subchambers are added to stack."""
+        chambers, chamber = stack_pop(chambers)
+        state = InstanceGeneratorState(maze, chambers, key)
+        maze, chambers, _ = split_vertically(state, chamber)
+
+        # Should contain (width - 1) wall tiles: full width except one passage
+        assert jnp.sum(maze == WALL) == self.WIDTH - 1
+        # Only one row must contain non 0 element
+        assert jnp.sum(jnp.any(maze, axis=1)) == 1
+
+        assert chambers.insertion_index >= 1
+
+    def test_split_horizontally(
+        self, maze: Maze, chambers: Stack, key: chex.PRNGKey
+    ) -> None:
+        """Test that a vertical wall is drawn and that subchambers are added to stack."""
+        chambers, chamber = stack_pop(chambers)
+        state = InstanceGeneratorState(maze, chambers, key)
+        maze, chambers, _ = split_horizontally(state, chamber)
+
+        # Should contain (height - 1) wall tiles: full height except one passage
+        assert jnp.sum(maze == WALL) == self.HEIGHT - 1
+        # Only one row must contain non 0 element
+        assert jnp.sum(jnp.any(maze, axis=0)) == 1
+
+        assert chambers.insertion_index >= 1
+
+    def test_generate_maze(self, key: chex.PRNGKey) -> None:
+        maze = generate_maze(self.WIDTH, self.HEIGHT, key)
+
+        # If the maze cannot be divided anymore and all tiles are connected, it is a valid maze
+        assert no_more_chamber(maze)
+        assert all_tiles_connected(maze)
+
+    def test_generate_maze_seed(self, key: chex.PRNGKey) -> None:
+        key1, key2 = jax.random.split(key)
+
+        maze1 = generate_maze(self.WIDTH, self.HEIGHT, key1)
+        maze2 = generate_maze(self.WIDTH, self.HEIGHT, key2)
+
+        assert jnp.any(maze1 != maze2)
+
+        maze3 = generate_maze(self.WIDTH, self.HEIGHT, key1)
+
+        assert jnp.all(maze1 == maze3)
