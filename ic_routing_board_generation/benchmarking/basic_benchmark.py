6f6ecaf11adcdbac3a4864220bc47f025acd084c
Marta <marta.wolinska16@imperial.ac.uk>
2023-02-22 16:53:42 +0000

    Add violin plots

--- ic_routing_board_generation/benchmarking/basic_benchmark.py
+++ ic_routing_board_generation/benchmarking/basic_benchmark.py
@@ -135,6 +135,40 @@ class BasicBenchmark:
                 stds=stds
             )
 
+    def master_plotting_loop_violin(self):
+        for board_size in self.benchmark_data.keys():
+            for parameter in ["total_wire_lengths", "proportion_wires_connected", "number_of_steps"]:
+                violin_chart_data = []
+                labels = []
+                for board_data in self.benchmark_data[board_size]:
+                    violin_chart_data.append(board_data.__getattribute__(parameter))
+                    labels.append(board_data.generator_type.generator_type.name)
+
+                self._plot_violin_plot(
+                    x_label="Board Generator", y_label=parameter,
+                    title=f"{parameter} over 1000 boards, {board_size}",
+                    data=np.array(violin_chart_data), labels=labels,
+                    file_name=f"/dist_{parameter}_{board_size}",
+                )
+
+    # def plot_total_wire_lengths_violin(self):
+    #     for board_size in self.benchmark_data.keys():
+    #         mean_lengths = []
+    #         labels = []
+    #         for benchmark in self.benchmark_data[board_size]:
+    #             labels.append(benchmark.generator_type.generator_type.value)
+    #             mean_lengths.append(benchmark.total_wire_lengths)
+    #
+    #         file_name = f"/total_wire_lenghts_{board_size}_violin" if self.save_plots else None
+    #         self._plot_violin_plot(
+    #             title=f"Total wire lengths over 1000 boards, {board_size}",
+    #             x_label="Generator type",
+    #             y_label="Total wire lengths",
+    #             data=mean_lengths,
+    #             labels=labels,
+    #             file_name=file_name
+    #         )
+
     def plot_proportion_wires_connected(self):
         for board_size in self.benchmark_data.keys():
             mean_proportion = []
@@ -207,10 +241,80 @@ class BasicBenchmark:
             xlabel=x_label,
             ylabel=y_label,
         )
+        data = np.array(data)
+
+        ax.set_xticks(labels)
+        ax.set_xticklabels(labels, rotation=30, ha='right')
+        inds = np.arange(0, len(data))
+        ax.scatter(inds, data, marker='o', color='k', s=30, zorder=3)
+        if stds is not None:
+            stds = np.array(stds)
+            ax.vlines(inds, data - (stds/2), data + (stds/2), color='blue', linestyle='-',
+                  lw=3)
+        plt.tight_layout()
         if file_name is not None:
             fig.savefig(str(self.directory_string) + str(file_name))
         else:
             plt.show()
+
+    def _plot_violin_plot(self,
+        x_label: str, y_label: str, title: str,
+        data: Iterable, labels: List[str],
+        file_name: Optional[str] = None
+    ):
+        # for i, el in enumerate(data):
+        #     plt.hist(el)
+        #     plt.title(i)
+        #     plt.show()
+        fig, ax = plt.subplots()
+        ax.violinplot(data, showmeans=True)
+        # ax.boxplot(data, showfliers=False)
+        ax.set(
+            title=title,
+            ylabel=y_label,
+        )
+
+        # quartile1, medians, quartile3 = np.percentile(data,
+        #                                               [25, 50, 75], axis=1)
+        # whiskers = np.array([
+        #     self._adjacent_values(sorted_array, q1, q3)
+        #     for sorted_array, q1, q3 in zip(data, quartile1, quartile3)])
+        # whiskers_min, whiskers_max = whiskers[:, 0], whiskers[:, 1]
+        #
+        # inds = np.arange(1, len(medians) + 1)
+        # ax.scatter(inds, medians, marker='o', color='white', s=30, zorder=3)
+        # ax.vlines(inds, quartile1, quartile3, color='k', linestyle='-', lw=5)
+        # ax.vlines(inds, whiskers_min, whiskers_max, color='k', linestyle='-',
+        #            lw=1)
+        # means = np.mean(data, axis =1)
+        # inds = np.arange(1, len(medians) + 1)
+        # # ax.scatter(inds, medians, marker='o', color='white', s=30, zorder=3)
+        # ax.scatter(inds, means, marker='x', color='k', s=30, zorder=3)
+        # ax.vlines(inds, quartile1, quartile3, color='blue', linestyle='-', lw=3)
+
+        self._set_axis_style(ax, labels, x_label)
+        plt.tight_layout()
+
+        if file_name is not None and self.save_plots:
+            fig.savefig(str(self.directory_string) + str(file_name))
+        else:
+            plt.show()
+            # print(means)
+            print()
+
+
+    def _adjacent_values(self, vals, q1, q3):
+        upper_adjacent_value = q3 + (q3 - q1) * 1.5
+        upper_adjacent_value = np.clip(upper_adjacent_value, q3, vals[-1])
+
+        lower_adjacent_value = q1 - (q3 - q1) * 1.5
+        lower_adjacent_value = np.clip(lower_adjacent_value, vals[0], q1)
+        return lower_adjacent_value, upper_adjacent_value
+
+    def _set_axis_style(self, ax, labels: List[str], x_axis_label: str = "Board gen"):
+        ax.set_xticks(np.arange(1, len(labels) + 1), labels=labels, rotation=30, ha='right')
+        ax.set_xlim(0.25, len(labels) + 0.75)
+        ax.set_xlabel(x_axis_label)
 
     @staticmethod
     def _process_raw_benchmark_data(raw_benchmark_data):
@@ -233,30 +337,35 @@ class BasicBenchmark:
 
 
 if __name__ == '__main__':
+    directory = "20230208_benchmark_23_45"
+    all_files = files_list_from_benchmark_experiment(directory)
+    benchmark = BasicBenchmark.from_file(all_files, directory_string=directory + "/")
+    print()
 
-    benchmarks = [
-        BoardGenerationParameters(rows=5, columns=5, number_of_wires=3,
-                                  generator_type=BoardName.BFS_2),
 
-
-    ]
-
-    # filenames = [
-    #     "BFS 2_8x8_agent_5.pkl",
-    #     "BFS_8x8_agent_5.pkl"
+    # benchmarks = [
+    #     BoardGenerationParameters(rows=5, columns=5, number_of_wires=3,
+    #                               generator_type=BoardName.BFS_2),
+    #
+    #
     # ]
-    # dir_string = Path(__file__).parent.parent.parent
-    # folder = "/ic_experiments/benchmarks/20230206_benchmark_14_03/"
-    # directory = str(dir_string) + folder
-
-    # benchmark = BasicBenchmark.from_file(filenames, directory)
-    benchmark = BasicBenchmark.from_simulation(benchmarks, 1, False)
-    print("bla")
-    for board_size in benchmark.benchmark_data.keys():
-        mean_proportion = []
-        labels = []
-        for benchmark in benchmark.benchmark_data[board_size]:
-            labels.append(benchmark.generator_type.generator_type)
+    #
+    # # filenames = [
+    # #     "BFS 2_8x8_agent_5.pkl",
+    # #     "BFS_8x8_agent_5.pkl"
+    # # ]
+    # # dir_string = Path(__file__).parent.parent.parent
+    # # folder = "/ic_experiments/benchmarks/20230206_benchmark_14_03/"
+    # # directory = str(dir_string) + folder
+    #
+    # # benchmark = BasicBenchmark.from_file(filenames, directory)
+    # benchmark = BasicBenchmark.from_simulation(benchmarks, 1, False)
+    # print("bla")
+    # for board_size in benchmark.benchmark_data.keys():
+    #     mean_proportion = []
+    #     labels = []
+    #     for benchmark in benchmark.benchmark_data[board_size]:
+    #         labels.append(benchmark.generator_type.generator_type)
 
 
 
