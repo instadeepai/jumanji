18ea0c4c7a4ef86e75d09b6f4dcee4f2e88bd95a
Marta <marta.wolinska16@imperial.ac.uk>
2023-03-07 14:43:22 +0000

    Update imports in interface

--- ic_routing_board_generation/interface/board_generator_interface.py
+++ ic_routing_board_generation/interface/board_generator_interface.py
@@ -3,34 +3,34 @@ from enum import Enum
 from ic_routing_board_generation.board_generator.bfs_board import BFSBoard
 from ic_routing_board_generation.board_generator.bfs_board_variations import \
     BFSBoardMinBends, BFSBoardFifo, BFSBoardShortest, BFSBoardLongest
-
-from ic_routing_board_generation.board_generator.board_generator_v1_1_2_rb import \
-    BoardV0
-from ic_routing_board_generation.board_generator.board_generator_v2_0_0_rb import \
-    RandomWalkBoard
+from ic_routing_board_generation.board_generator.board_generator_v3_1_0_rb import \
+    Board_rb
+from ic_routing_board_generation.board_generator.lsystem_board import \
+    LSystemBoardGen
 
 
 class BoardName(str, Enum):
     """Enum of implemented board generators."""
-    RANDOM_V0 = "random_v0"
-    RANDOM_WALK = "random_walk"
+    # RANDOM_V3 = "random_v3"
     BFS_BASE = "BFS_base"
     BFS_MIN_BENDS = "bfs_min_bend"
     BFS_FIFO = "bfs_fifo"
     BFS_SHORTEST = "bfs_short"
     BFS_LONGEST = "bfs_long"
+    LSYSTEMS = "lsystems_standard"
 
 
 class BoardGenerator:
     """Maps BoardGeneratorType to class of generator."""
     board_generator_dict = {
-        BoardName.RANDOM_V0: BoardV0,
-        BoardName.RANDOM_WALK: RandomWalkBoard,
+        # BoardName.RANDOM_V3: Board_rb,
         BoardName.BFS_BASE: BFSBoard,
         BoardName.BFS_MIN_BENDS: BFSBoardMinBends,
         BoardName.BFS_FIFO: BFSBoardFifo,
         BoardName.BFS_SHORTEST: BFSBoardShortest,
         BoardName.BFS_LONGEST: BFSBoardLongest,
+        BoardName.LSYSTEMS: LSystemBoardGen,
+
     }
 
     @classmethod
