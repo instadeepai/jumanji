5a6f6f9fe07584e64dc56b786ad7d1e489718245
Marta <marta.wolinska16@imperial.ac.uk>
2023-03-08 12:45:40 +0000

    Add changes to abstract board

--- ic_routing_board_generation/board_generator/abstract_board.py
+++ ic_routing_board_generation/board_generator/abstract_board.py
@@ -7,8 +7,12 @@ class AbstractBoard(ABC):
     def __init__(self, rows: int, cols: int, num_agents: int):
         self._rows = rows
         self._cols = cols
-        self._num_agents = num_agents
+        self._num_agents = num_agents # upper limit
+        self._wires_on_board = 0
 
+    @abstractmethod
+    def _is_num_agents_hit(self):
+        return self.num_agents == self._wires_on_board
     @property
     def rows(self):
         return self._rows
@@ -33,6 +37,14 @@ class AbstractBoard(ABC):
     def num_agents(self, value):
         self._num_agents = value
 
+    @property
+    def wires_on_board(self):
+        return self._wires_on_board
+
     @abstractmethod
     def return_training_board(self) -> np.ndarray:
+        pass
+
+    @abstractmethod
+    def return_solved_board(self) -> np.ndarray:
         pass
