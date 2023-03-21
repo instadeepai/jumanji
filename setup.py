5be9686c173ce0eb076c49b7d183d7953819b8d9
Clément Bonnet <56230714+clement-bonnet@users.noreply.github.com>
2023-03-21 12:25:47 +0000

    feat: move __version__ to jumanji/version.py and expose it in jumanji… (#78)

--- setup.py
+++ setup.py
@@ -18,10 +18,6 @@ from typing import List
 import setuptools
 from setuptools import setup
 
-__version__ = "0.2.0"
-
-_CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
-
 
 def _parse_requirements(path: str) -> List[str]:
     """Returns content of given requirements file."""
@@ -31,29 +27,31 @@ def _parse_requirements(path: str) -> List[str]:
         ]
 
 
+def _get_version() -> str:
+    """Grabs the package version from jumanji/version.py."""
+    dict_ = {}
+    with open("jumanji/version.py") as f:
+        exec(f.read(), dict_)
+    return dict_["__version__"]
+
+
 setup(
     name="jumanji",
-    version=__version__,
+    version=_get_version(),
     author="InstaDeep",
     author_email="hello@instadeep.com",
-    description="Industry-Driven Hardware-Accelerated RL Environments",
+    description="A suite of diverse and challenging RL environments in JAX",
     license="Apache 2.0",
     url="https://github.com/instadeepai/jumanji/",
-    long_description=open(os.path.join(_CURRENT_DIR, "README.md")).read(),
+    long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     keywords="reinforcement-learning python jax",
     packages=setuptools.find_packages(),
     python_requires=">=3.8",
-    install_requires=_parse_requirements(
-        os.path.join(_CURRENT_DIR, "requirements", "requirements.txt")
-    ),
+    install_requires=_parse_requirements("requirements/requirements.txt"),
     extras_require={
-        "dev": _parse_requirements(
-            os.path.join(_CURRENT_DIR, "requirements", "requirements-dev.txt")
-        ),
-        "train": _parse_requirements(
-            os.path.join(_CURRENT_DIR, "requirements", "requirements-train.txt")
-        ),
+        "dev": _parse_requirements("requirements/requirements-dev.txt"),
+        "train": _parse_requirements("requirements/requirements-train.txt"),
     },
     package_data={"jumanji": ["py.typed"]},
     classifiers=[
