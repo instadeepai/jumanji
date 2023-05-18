5c12096414f8c1406e098f4d81967b215d020970
surana01 <s.surana@instadeep.com>
2023-05-18 11:58:22 +0100

    feat(cvrp): generator

--- jumanji/environments/routing/cvrp/conftest.py
+++ jumanji/environments/routing/cvrp/conftest.py
@@ -12,10 +12,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import jax
+import jax.numpy as jnp
 import pytest
+from chex import PRNGKey
 
 from jumanji.environments.routing.cvrp.env import CVRP
+from jumanji.environments.routing.cvrp.generator import Generator, RandomGenerator
 from jumanji.environments.routing.cvrp.reward import DenseReward, SparseReward
+from jumanji.environments.routing.cvrp.types import State
 
 
 @pytest.fixture
@@ -33,7 +38,10 @@ def cvrp_dense_reward(dense_reward: DenseReward) -> CVRP:
     """Instantiates a CVRP environment with dense rewards and 5 nodes, maximum capacity of 3
     and maximum demand of 2.
     """
-    return CVRP(num_nodes=5, max_capacity=3, max_demand=2, reward_fn=dense_reward)
+    return CVRP(
+        generator=RandomGenerator(num_nodes=5, max_capacity=3, max_demand=2),
+        reward_fn=dense_reward,
+    )
 
 
 @pytest.fixture
@@ -41,4 +49,53 @@ def cvrp_sparse_reward(sparse_reward: SparseReward) -> CVRP:
     """Instantiates a CVRP environment with sparse rewards and 5 nodes, maximum capacity of 3
     and maximum demand of 2.
     """
-    return CVRP(num_nodes=5, max_capacity=3, max_demand=2, reward_fn=sparse_reward)
+    return CVRP(
+        generator=RandomGenerator(num_nodes=5, max_capacity=3, max_demand=2),
+        reward_fn=sparse_reward,
+    )
+
+
+class DummyGenerator(Generator):
+    """Hardcoded `Generator` mainly used for testing and debugging. It deterministically outputs a
+    hardcoded instance with 4 nodes (cities), maximum vehcicle capacity of 6 and maximum city demand
+    of 3.
+    """
+
+    def __init__(self) -> None:
+        super().__init__(
+            num_nodes=4,
+            max_capacity=6,
+            max_demand=3,
+        )
+
+    def __call__(self, key: PRNGKey) -> State:
+        del key
+
+        coordinates = jnp.array(
+            [[0.0, 0.0], [0.0, 1.0], [1.0, 0.0], [1.0, 1.0], [0.5, 0.5]]
+        )
+        demands = jnp.array([0, 1, 2, 1, 2])
+
+        # The initial position is set at the depot.
+        position = jnp.array(0, jnp.int32)
+        num_total_visits = jnp.array(1, jnp.int32)
+
+        # The initial capacity is set to the maximum capacity.
+        capacity = jnp.array(6, jnp.int32)
+
+        # Initially, the agent has only visited the depot.
+        visited_mask = jnp.array([True, False, False, False, False], bool)
+        trajectory = jnp.array([0, 0, 0, 0, 0, 0, 0, 0], jnp.int32)
+
+        state = State(
+            coordinates=coordinates,
+            demands=demands,
+            position=position,
+            capacity=capacity,
+            visited_mask=visited_mask,
+            trajectory=trajectory,
+            num_total_visits=num_total_visits,
+            key=jax.random.PRNGKey(0),
+        )
+
+        return state
