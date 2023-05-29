fix: fix action spec off by one.

jumanji/environments/packing/jigsaw/env.py
@@ -291,8 +291,8 @@ class Jigsaw(Environment[State]):
             shape=(
                 self.num_pieces,
                 4,
-                self.num_rows - 3,
-                self.num_cols - 3,
+                self.num_rows - 2,
+                self.num_cols - 2,
             ),
             minimum=False,
             maximum=True,
@@ -320,8 +320,8 @@ class Jigsaw(Environment[State]):
             - max_col_position: int between 0 and max_col_position - 1 (included).
         """
 
-        max_row_position = self.num_rows - 3
-        max_col_position = self.num_cols - 3
+        max_row_position = self.num_rows - 2
+        max_col_position = self.num_cols - 2
 
         return specs.MultiDiscreteArray(
             num_values=jnp.array(
@@ -478,8 +478,8 @@ class Jigsaw(Environment[State]):
         num_pieces, num_rotations, num_rows, num_cols = (
             self.num_pieces,
             4,
-            self.num_rows - 3,
-            self.num_cols - 3,
+            self.num_rows - 2,
+            self.num_cols - 2,
         )
 
         pieces_grid, rotations_grid, rows_grid, cols_grid = jnp.meshgrid(

jumanji/environments/packing/jigsaw/generator.py
@@ -297,7 +297,7 @@ class RandomJigsawGenerator(InstanceGenerator):
             col_nibs_idxs=col_nibs_idxs,
             row_nibs_idxs=row_nibs_idxs,
             action_mask=jnp.ones(
-                (num_pieces, 4, grid_row_dim - 3, grid_col_dim - 3), dtype=bool
+                (num_pieces, 4, grid_row_dim - 2, grid_col_dim - 2), dtype=bool
             ),
             current_board=jnp.zeros_like(solved_board),
             step_count=0,
@@ -342,7 +342,7 @@ class ToyJigsawGeneratorWithRotation(InstanceGenerator):
             solved_board=mock_solved_grid,
             pieces=mock_pieces,
             current_board=jnp.zeros_like(mock_solved_grid),
-            action_mask=jnp.ones((4, 4, 2, 2), dtype=bool),
+            action_mask=jnp.ones((4, 4, 3, 3), dtype=bool),
             col_nibs_idxs=jnp.array([2], dtype=jnp.int32),
             row_nibs_idxs=jnp.array([2], dtype=jnp.int32),
             num_pieces=jnp.int32(4),
@@ -394,7 +394,7 @@ class ToyJigsawGeneratorNoRotation(InstanceGenerator):
             row_nibs_idxs=jnp.array([2], dtype=jnp.int32),
             num_pieces=jnp.int32(4),
             key=jax.random.PRNGKey(0),
-            action_mask=jnp.ones((4, 4, 2, 2), dtype=bool),
+            action_mask=jnp.ones((4, 4, 3, 3), dtype=bool),
             current_board=jnp.zeros_like(mock_solved_grid),
             step_count=0,
             placed_pieces=jnp.zeros(4, dtype=bool),

jumanji/training/networks/jigsaw/actor_critic.py
@@ -83,7 +83,7 @@ class UNet(hk.Module):
 
         # Crop the upconvolved output
         # to be the same size as the action mask.
-        up_2 = up_2[:, 1:-2, 1:-2]
+        up_2 = up_2[:, 1:-1, 1:-1]
         # Remove the channel dimension
         output = jnp.squeeze(up_2, axis=-1)
 
@@ -126,7 +126,7 @@ class JigsawTorso(hk.Module):
 
         unet = UNet()
         board_conv_encoding, board_encoding = unet(observation.current_board)
-        # board_encoding has shape (B, num_rows-3, num_cols-3)
+        # board_encoding has shape (B, num_rows-2, num_cols-2)
 
         # Flatten the board_conv_encoding so it is of shape (B, num_maps, ...)
         board_conv_encoding = jnp.reshape(
@@ -155,7 +155,7 @@ class JigsawTorso(hk.Module):
         pieces_embedding = jax.vmap(pieces_head)(pieces_embedding)
 
         # pieces_embedding has shape (B, num_pieces, num_rotations)
-        # board_encoding has shape (B, num_rows-3, num_cols-3) to match
+        # board_encoding has shape (B, num_rows-2, num_cols-2) to match
         # the shape of the action mask.
         return pieces_embedding, board_encoding
 
@@ -177,7 +177,6 @@ def make_actor_network_jigsaw(
             name="policy_torso",
         )
         pieces_embedding, board_embedding = torso(observation)
-
         # Outer-product
         outer_product = jnp.einsum(
             "...ij,...kl->...ijkl", pieces_embedding, board_embedding