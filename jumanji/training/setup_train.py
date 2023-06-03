0bd9352b3254acdb6f6597c2bf80f1605490b07b
Clement Bonnet <clement.bonnet16@gmail.com>
2023-06-03 18:55:42 +0100

    fix: linter

--- jumanji/training/setup_train.py
+++ jumanji/training/setup_train.py
@@ -343,7 +343,7 @@ def _setup_actor_critic_neworks(  # noqa: CCR001
             conv_num_channels=cfg.env.network.conv_num_channels,
             tetromino_layers=cfg.env.network.tetromino_layers,
             final_layer_dims=cfg.env.network.final_layer_dims,
-    )
+        )
     elif cfg.env.name == "mmst":
         assert isinstance(env.unwrapped, MMST)
         actor_critic_networks = networks.make_actor_critic_networks_mmst(
