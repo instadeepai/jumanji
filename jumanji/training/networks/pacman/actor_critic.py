13d9f305a00baa60f1b3c2275c0b3e5538c00b0e
siddarthsingh1 <s.singh@instadeep.com>
2023-07-07 18:17:34 +0200

    feat: increase obs size

--- jumanji/training/networks/pacman/actor_critic.py
+++ jumanji/training/networks/pacman/actor_critic.py
@@ -129,7 +129,9 @@ def process_image(observation: Observation) -> chex.Array:
     layer_3 = layer_3.at[0, 0].set(0)
     obs = [layer_1, layer_2, layer_3]
     rgb = jnp.stack(obs, axis=-1)
-    return rgb
+    n = 3
+    expand_rgb = jax.numpy.kron(rgb, jnp.ones((n, n)))
+    return expand_rgb
 
 
 def make_network_pacman(
