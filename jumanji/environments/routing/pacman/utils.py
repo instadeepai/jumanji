d7323b8c560900cb54a4946d4fc73aff70e86778
siddarthsingh1 <s.singh@instadeep.com>
2023-07-17 14:49:06 +0200

    chore: renderer update

--- jumanji/environments/routing/pacman/utils.py
+++ jumanji/environments/routing/pacman/utils.py
@@ -79,23 +79,24 @@ def create_grid_image(observation: Union[Observation, State]) -> chex.Array:
         rgb: A 3-dimensional array representing the RGB observation of the current state.
     """
 
-    layer_1 = observation.grid * 0.66
-    layer_2 = observation.grid * 0.0
-    layer_3 = observation.grid * 0.33
+    # Make walls blue and passages black
+    layer_1 = (1 - observation.grid) * 0.0
+    layer_2 = (1 - observation.grid) * 0.0
+    layer_3 = (1 - observation.grid) * 0.6
+
     player_loc = observation.player_locations
     ghost_pos = observation.ghost_locations
     pellets_loc = observation.power_up_locations
     is_scared = observation.frightened_state_time
     idx = observation.pellet_locations
-
     n = 3
 
-    # Power pellet is purple
+    # Power pellet are pink
     for i in range(len(pellets_loc)):
         p = pellets_loc[i]
-        layer_1 = layer_1.at[p[1], p[0]].set(0.5)
-        layer_2 = layer_2.at[p[1], p[0]].set(0)
-        layer_3 = layer_3.at[p[1], p[0]].set(0.5)
+        layer_1 = layer_1.at[p[1], p[0]].set(1.0)
+        layer_2 = layer_2.at[p[1], p[0]].set(0.8)
+        layer_3 = layer_3.at[p[1], p[0]].set(0.6)
 
     # Set player is yellow
     layer_1 = layer_1.at[player_loc.x, player_loc.y].set(1)
@@ -103,8 +104,8 @@ def create_grid_image(observation: Union[Observation, State]) -> chex.Array:
     layer_3 = layer_3.at[player_loc.x, player_loc.y].set(0)
 
     cr = jnp.array([1, 1, 0, 1])
-    cg = jnp.array([0, 0.7, 1, 0.7])
-    cb = jnp.array([0, 1, 1, 0.35])
+    cg = jnp.array([0, 0.7, 1, 0.5])
+    cb = jnp.array([0, 1, 1, 0.0])
     # Set ghost locations
 
     layers = (layer_1, layer_2, layer_3)
@@ -141,7 +142,7 @@ def create_grid_image(observation: Union[Observation, State]) -> chex.Array:
 
     layer_1 = layer_1.at[0, 0].set(0)
     layer_2 = layer_2.at[0, 0].set(0)
-    layer_3 = layer_3.at[0, 0].set(0)
+    layer_3 = layer_3.at[0, 0].set(0.6)
 
     obs = [layer_1, layer_2, layer_3]
     rgb = jnp.stack(obs, axis=-1)
@@ -151,18 +152,19 @@ def create_grid_image(observation: Union[Observation, State]) -> chex.Array:
     layer_2 = expand_rgb[:, :, 1]
     layer_3 = expand_rgb[:, :, 2]
 
+    # place normal pellets
     for i in range(len(idx)):
         if jnp.array(idx[i]).sum != 0:
             loc = idx[i]
             c = loc[1] * n + 1
             r = loc[0] * n + 1
-            layer_3 = layer_3.at[c, r].set(1)
+            layer_1 = layer_1.at[c, r].set(1.0)
             layer_2 = layer_2.at[c, r].set(0.8)
-            layer_1 = layer_1.at[c, r].set(0.6)
+            layer_3 = layer_3.at[c, r].set(0.6)
 
     layers = (layer_1, layer_2, layer_3)
 
-    # cover patches
+    # Draw details
     def set_ghost_colours2(
         layers: chex.Array,
     ) -> Tuple[chex.Array, chex.Array, chex.Array]:
@@ -176,6 +178,26 @@ def create_grid_image(observation: Union[Observation, State]) -> chex.Array:
             layer_1 = layer_1.at[c, r].set(cr[i])
             layer_2 = layer_2.at[c, r].set(cg[i])
             layer_3 = layer_3.at[c, r].set(cb[i])
+
+            # Make notch in top
+            layer_1 = layer_1.at[c - 1, r - 1].set(0.0)
+            layer_2 = layer_2.at[c - 1, r - 1].set(0.0)
+            layer_3 = layer_3.at[c - 1, r - 1].set(0.0)
+
+            # Make notch in top
+            layer_1 = layer_1.at[c - 1, r + 1].set(0.0)
+            layer_2 = layer_2.at[c - 1, r + 1].set(0.0)
+            layer_3 = layer_3.at[c - 1, r + 1].set(0.0)
+
+            # Eyes
+            layer_1 = layer_1.at[c, r + 1].set(1)
+            layer_2 = layer_2.at[c, r + 1].set(1)
+            layer_3 = layer_3.at[c, r + 1].set(1)
+
+            layer_1 = layer_1.at[c, r - 1].set(1)
+            layer_2 = layer_2.at[c, r - 1].set(1)
+            layer_3 = layer_3.at[c, r - 1].set(1)
+
         return layer_1, layer_2, layer_3
 
     def set_ghost_colours_scared2(
@@ -185,9 +207,33 @@ def create_grid_image(observation: Union[Observation, State]) -> chex.Array:
         for i in range(4):
             y = ghost_pos[i][0]
             x = ghost_pos[i][1]
+
+            c = x * n + 1
+            r = y * n + 1
+
             layer_1 = layer_1.at[x * n + 1, y * n + 1].set(0)
             layer_2 = layer_2.at[x * n + 1, y * n + 1].set(0)
             layer_3 = layer_3.at[x * n + 1, y * n + 1].set(1)
+
+            # Make notch in top
+            layer_1 = layer_1.at[c - 1, r - 1].set(0.0)
+            layer_2 = layer_2.at[c - 1, r - 1].set(0.0)
+            layer_3 = layer_3.at[c - 1, r - 1].set(0.0)
+
+            # Make notch in top
+            layer_1 = layer_1.at[c - 1, r + 1].set(0.0)
+            layer_2 = layer_2.at[c - 1, r + 1].set(0.0)
+            layer_3 = layer_3.at[c - 1, r + 1].set(0.0)
+
+            # Eyes
+            layer_1 = layer_1.at[c, r + 1].set(1)
+            layer_2 = layer_2.at[c, r + 1].set(0.6)
+            layer_3 = layer_3.at[c, r + 1].set(0.2)
+
+            layer_1 = layer_1.at[c, r - 1].set(1)
+            layer_2 = layer_2.at[c, r - 1].set(0.6)
+            layer_3 = layer_3.at[c, r - 1].set(0.2)
+
         return layer_1, layer_2, layer_3
 
     layers = jax.lax.cond(
@@ -196,12 +242,12 @@ def create_grid_image(observation: Union[Observation, State]) -> chex.Array:
 
     layer_1, layer_2, layer_3 = layers
 
-    # Power pellet is purple
+    # Power pellet is pink
     for i in range(len(pellets_loc)):
         p = pellets_loc[i]
-        layer_1 = layer_1.at[p[1] * n + 1, p[0] * n + 1].set(0.5)
-        layer_2 = layer_2.at[p[1] * n + 1, p[0] * n + 1].set(0)
-        layer_3 = layer_3.at[p[1] * n + 1, p[0] * n + 1].set(0.5)
+        layer_1 = layer_1.at[p[1] * n + 2, p[0] * n + 1].set(1)
+        layer_2 = layer_2.at[p[1] * n + 1, p[0] * n + 1].set(0.8)
+        layer_3 = layer_3.at[p[1] * n + 1, p[0] * n + 1].set(0.6)
 
     # Set player is yellow
     layer_1 = layer_1.at[player_loc.x * n + 1, player_loc.y * n + 1].set(1)
