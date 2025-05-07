8d8e5b0afb1b134ed466437d47f52936dcddb71d
Sasha Abramowitz <reallysasha@gmail.com>
2025-05-07 11:25:34 +0200

    feat: tests for new connector rewards

--- jumanji/environments/routing/connector/reward.py
+++ jumanji/environments/routing/connector/reward.py
@@ -13,12 +13,11 @@
 # limitations under the License.
 
 import abc
-from typing import Tuple, override
 
 import chex
 import jax.numpy as jnp
+from typing_extensions import override
 
-from jumanji import specs
 from jumanji.environments.routing.connector.types import State
 
 
@@ -43,24 +42,15 @@ class RewardFn(abc.ABC):
             The reward for the current step.
         """
 
-    @property
-    @abc.abstractmethod
-    def shape(self) -> Tuple[int, ...]: ...
 
-    def spec(self) -> specs.Array:
-        return specs.Array(shape=self.shape, dtype=jnp.float32, name="reward")
-
-
-class MultiAgentDenseRewardFn(RewardFn):
+class DenseRewardFn(RewardFn):
     """Returns: reward of 1.0 for each agent that connects on that step and adds a penalty of
     -0.03, per agent, at every timestep where they have yet to connect.
 
     The reward is of shape num_agents where a value in dimension 1 corresponds agent 1's reward.
     """
 
-    def __init__(
-        self, num_agents: int, connected_reward: float = 1.0, timestep_reward: float = -0.03
-    ) -> None:
+    def __init__(self, connected_reward: float = 1.0, timestep_reward: float = -0.03) -> None:
         """Instantiates a dense reward function for the `Connector` environment.
 
         Args:
@@ -68,7 +58,6 @@ class MultiAgentDenseRewardFn(RewardFn):
             connected_reward: reward agent if it connects on that step.
             timestep_reward: reward penalty for every timestep, encourages agent to connect quickly.
         """
-        self.num_agents = num_agents
         self.timestep_reward = timestep_reward
         self.connected_reward = connected_reward
 
@@ -84,58 +73,35 @@ class MultiAgentDenseRewardFn(RewardFn):
         timestep_rewards = self.timestep_reward * jnp.asarray(~state.agents.connected, jnp.float32)
         return connected_rewards + timestep_rewards
 
-    @property
-    def shape(self) -> Tuple[int, ...]:
-        return (self.num_agents,)
 
-
-class SingleAgentDenseRewardFn(MultiAgentDenseRewardFn):
-    """Returns: the sum of agents of the `MultiAgentDenseRewardFn`.
-
-    The reward is a scalar, so all agents get the same (shared) reward.
-    """
-
-    def __init__(self) -> None: ...
+class SharedDenseRewardFn(DenseRewardFn):
+    """Returns: the sum of agents of the `MultiAgentDenseRewardFn`."""
 
     @override
     def __call__(self, state: State, action: chex.Array, next_state: State) -> chex.Array:
-        return jnp.sum(super().__call__(state, action, next_state), axis=-1)
-
-    @property
-    def shape(self) -> Tuple[int, ...]:
-        return ()
+        individual_reward = super().__call__(state, action, next_state)
+        num_agents = individual_reward.shape[0]
+        return jnp.sum(individual_reward, axis=-1, keepdims=True).repeat(num_agents, axis=-1)
 
 
-class MultiAgentSparseRewardFn(RewardFn):
+class SparseRewardFn(RewardFn):
     """Returns: a reward of 1 for each agent that connects on the current step.
 
     The reward is of shape num_agents where a value in dimension 1 corresponds agent 1's reward.
     """
 
-    def __init__(self, num_agents: int) -> None:
-        self.num_agents = num_agents
-
     @override
     def __call__(self, state: State, action: chex.Array, next_state: State) -> chex.Array:
         return jnp.asarray(~state.agents.connected & next_state.agents.connected, jnp.float32)
 
-    @property
-    def shape(self) -> Tuple[int, ...]:
-        return (self.num_agents,)
 
-
-class SingleAgentSparseRewardFn(MultiAgentSparseRewardFn):
-    """Returns: a reward of 1 for any agent that connects on the current step.
-
-    The reward is a scalar, so all agents get the same (shared) reward.
-    """
+class SharedSparseRewardFn(SparseRewardFn):
+    """Returns: a reward of 1 for any agent that connects on the current step."""
 
     def __init__(self) -> None: ...
 
     @override
     def __call__(self, state: State, action: chex.Array, next_state: State) -> chex.Array:
-        return jnp.sum(super().__call__(state, action, next_state), axis=-1)
-
-    @property
-    def shape(self) -> Tuple[int, ...]:
-        return ()
+        individual_reward = super().__call__(state, action, next_state)
+        num_agents = individual_reward.shape[0]
+        return jnp.sum(individual_reward, axis=-1, keepdims=True).repeat(num_agents, axis=-1)
